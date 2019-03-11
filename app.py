0282a1d7fd6a0b0958e1be86d3412dee1fa5ec96
George Higbie <ghigbie@monigle.com>
2019-03-11 09:21:11 -0600

    Add tempaltes routes

--- app.py
+++ app.py
@@ -7,9 +7,18 @@ title = "Flask Forms"
 def index():
     return render_template('home.html', title=title)
 
+@app.route('signup')
+def signup():
+    return render_template('signup.html', title=title)
+
+@app.route('thankyou')
+def thankyou():
+    return render_template('thankyou.html', title=title)
+
 @app.route('/about')
 def about():
     return render_template('about.html', title=title)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
